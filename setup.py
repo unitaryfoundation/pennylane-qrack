4f66552b9a0b457a405a1ad27b20fd187f3509c0
WrathfulSpatula <stranoj@gmail.com>
2025-08-02 17:05:24 -0400

    Declare stabilizer plugin

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ info = {
     "packages": ["pennylane_qrack"],
     "cmdclass": {"build_py": Build},
     "entry_points": {
-        "pennylane.plugins": ["qrack.simulator = pennylane_qrack.qrack_device:QrackDevice", "qrack.ace = pennylane_qrack.qrack_ace_device:QrackAceDevice"]
+        "pennylane.plugins": ["qrack.simulator = pennylane_qrack.qrack_device:QrackDevice", "qrack.ace = pennylane_qrack.qrack_ace_device:QrackAceDevice", "qrack.stabilizer = pennylane_qrack.qrack_stabilizer_device:QrackStabilizerDevice"]
     },
     "description": "PennyLane plugin for Qrack.",
     "long_description": open("README.rst").read(),
